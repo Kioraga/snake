78084f0b6bd30c8b1586f79ab74281003eb58e90
DanielCancelas <dcancelasgarcia@danielcastelao.org>
2020-03-04 11:19:45 +0100

    Some changes

--- src/snake/Snake.java
+++ src/snake/Snake.java
@@ -38,26 +38,61 @@ public class Snake {
         Image background = new Image("file:resources/background.png");
         Image snake = new Image("file:resources/snake.png");
 
-        Tablero[] tablero = new Tablero[20];
-        tablero[0] = new Tablero(30, 60);
-        for (int i = 1; i < tablero.length; i++) {
-            tablero[i] = new Tablero(tablero[i-1].getX(), tablero[i-1].getY());
-        }
+        Tablero[][] tablero = Tablero.crearTablero();
+
+        int time = 30;
+        final int[] dir = {4};
+        final int[] f = {9};
+        final int[] c = {5};
+        final double[] x = {tablero[f[0]][c[0]].getX()};
+        final double[] y = {tablero[f[0]][c[0]].getY()};
 
         new AnimationTimer() {
             public void handle(long now) {
+                if ((input.contains("W") || input.contains("UP"))  && dir[0] != 3)
+                    dir[0] = 1;
+                if ((input.contains("A") || input.contains("LEFT")) && dir[0] != 4)
+                    dir[0] = 2;
+                if ((input.contains("S") || input.contains("DOWN")) && dir[0] != 1)
+                    dir[0] = 3;
+                if ((input.contains("D") || input.contains("RIGHT")) && dir[0] != 2)
+                    dir[0] = 4;
 
-                /*if (input.contains("W"))
-                    y[0] = y[0] - 25;
-                if (input.contains("A"))
-                    x[0] = x[0] - 25;
-                if (input.contains("S"))
-                    y[0] = y[0] + 25;
-                if (input.contains("D"))
-                    x[0] = x[0] + 25;*/
+                switch (dir[0]) {
+                    case 1:
+                        if (f[0] > 0) {
+                            f[0] = f[0] - 1;
+                            y[0] = tablero[f[0]][c[0]].getY();
+                        }
+                        break;
+                    case 2:
+                        if (c[0] > 0) {
+                            c[0] = c[0] - 1;
+                            x[0] = tablero[f[0]][c[0]].getX();
+                        }
+                        break;
+                    case 3:
+                        if (f[0] < 19) {
+                            f[0] = f[0] + 1;
+                            y[0] = tablero[f[0]][c[0]].getY();
+                        }
+                        break;
+                    case 4:
+                        if (c[0] < 19) {
+                            c[0] = c[0] + 1;
+                            x[0] = tablero[f[0]][c[0]].getX();
+                        }
+                        break;
+                }
 
                 gc.drawImage(background, 0, 0);
-                //gc.drawImage(snake, x[0], y[0]);
+                gc.drawImage(snake, x[0], y[0]);
+
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }.start();
 
--- src/snake/Tablero.java
+++ src/snake/Tablero.java
@@ -1,7 +1,5 @@
 package snake;
 
-import java.util.ArrayList;
-
 public class Tablero {
     private double x;
     private double y;
@@ -25,5 +23,30 @@ public class Tablero {
     }
     public void setY(double y) {
         this.y = y;
+    }
+
+    public static Tablero[][] crearTablero() {
+        Tablero[][] tablero = new Tablero[20][20];
+
+        double iX = 30;
+        double iY = 60;
+        for (int i = 0; i < 20; i++) {
+            tablero[i][0] = new Tablero(iX, iY);
+            for (int j = 1; j < 20; j++) {
+                tablero[i][j] = new Tablero(tablero[i][j-1].getX()+25, iY);
+            }
+            iY = iY + 25;
+        }
+
+        return tablero;
+    }
+
+    public static void mostrarTablero(Tablero[][] tablero) {
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero.length; j++) {
+                System.out.print(tablero[i][j].getX()+" "+tablero[i][j].getY()+" , ");
+            }
+            System.out.println("");
+        }
     }
 }
