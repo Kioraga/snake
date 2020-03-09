cc2cd63097e419cbaf73cca99775e66c41e3d256
dcancelas <55793127+DanielCancelas@users.noreply.github.com>
2020-03-09 16:58:32 +0100

    Some changes

--- src/snake/Main.java
+++ src/snake/Main.java
@@ -17,19 +17,16 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Snake by dcancelas");
-        primaryStage.setScene(Main.titleScreen(false));
+        primaryStage.setScene(Main.titleScreen());
         primaryStage.setResizable(false);
         primaryStage.show();
 
         pStage = primaryStage;
     }
 
-    public static Scene titleScreen(boolean start) throws Exception {
+    public static Scene titleScreen() throws Exception {
         Parent root = FXMLLoader.load(Main.class.getResource("menu.fxml"));
-        if (start)
-            return new Scene(root, 550, 580);
-        else
-            return new Scene(root, 560, 590);
+        return new Scene(root, 550, 580);
     }
 
     public static Stage getPrimaryStage() {
